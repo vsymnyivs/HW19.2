82c44f2f90204c591fed1f96fef37cc5afc8343d
vsymnyivs <vsymnyivs@gmail.com>
2024-06-27 00:25:15 +0300

    edit home work 20.1 - complit dump data and I rewrote the Cyrillic alphabet into the Latin alphabet

--- catalog/models.py
+++ catalog/models.py
@@ -5,30 +5,30 @@ NULLABLE = {'null': True, 'blank': True}
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=250, verbose_name='Наименование')
-    description = models.TextField(verbose_name='описание', **NULLABLE)
+    name = models.CharField(max_length=250, verbose_name='Name')
+    description = models.TextField(verbose_name='description', **NULLABLE)
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=250, verbose_name='Наименование')
-    description = models.TextField(verbose_name='описание', **NULLABLE)
-    image = models.ImageField(upload_to='catalog/', verbose_name='описание', **NULLABLE)
-    price = models.FloatField(verbose_name='цена')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
-    created_at = models.DateTimeField(default=timezone.now, verbose_name='Дата заполнения')
-    update_at = models.DateTimeField(default=timezone.now, verbose_name='Дата изменения')
+    name = models.CharField(max_length=250, verbose_name='Name')
+    description = models.TextField(verbose_name='description', **NULLABLE)
+    image = models.ImageField(upload_to='catalog/', verbose_name='description', **NULLABLE)
+    price = models.FloatField(verbose_name='price')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Category')
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='Date of creation')
+    update_at = models.DateTimeField(default=timezone.now, verbose_name='Date of Update')
     #manufactured_at = models.DateTimeField(default=timezone.now, verbose_name='Дата производства продукта')
 
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 
     def __str__(self):
         return f'{self.name}{self.price}'
