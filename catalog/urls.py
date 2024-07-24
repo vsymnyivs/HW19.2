a26181984c4e616c2552052355b1af503f65b400
vsymnyivs <vsymnyivs@gmail.com>
2024-07-25 00:05:48 +0300

    home work 22.1 - complate home work.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ContactsTemplateView, HomeListView, ProductDetailView
+from catalog.views import ContactsTemplateView, HomeListView, ProductDetailView, ProductDeleteView, ProductCreateView, \
+    ProductUpdateView
 
 app_name = CatalogConfig.name
 
@@ -8,4 +9,7 @@ urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
     path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_view'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete'),
+    path('create/', ProductCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', ProductUpdateView.as_view(), name='update'),
 ]
---
+++ common/views.py
@@ -0,0 +1,15 @@
+from django.forms import BooleanField
+
+
+class StyleFormMixin:
+        """
+        Миксин для стилизации форм.
+        """
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                if isinstance(field, BooleanField):
+                    field.widget.attrs['class'] = 'form-check-input'
+                else:
+                    field.widget.attrs['class'] = 'form-control'
